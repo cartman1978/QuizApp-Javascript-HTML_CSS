2334d4d3f2c01ab4df33cba1489177c08ff1d09a
Fabio Torti <email>
2021-03-13 11:32:53 +0000

    added function to show feedback on correct or incorrect answer

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,14 +66,25 @@ getNewQuestion = () => {
 };
 
 choices.forEach((choice) => {
-    choice.addEventListener('click', (e) => {
+    choice.addEventListener('click', e => {
         if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
-        console.log(selectedAnswer);
-        getNewQuestion();
+
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+
+
     });
 });
 
