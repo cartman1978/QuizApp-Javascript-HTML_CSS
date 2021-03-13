650e895c3b39753930f06ce985b1b4563ab219f1
Fabio Torti <email>
2021-03-13 09:53:39 +0000

    added getnew question function

--- assets/js/game.js
+++ assets/js/game.js
@@ -49,7 +49,15 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
 
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
 }
 
 startGame();
