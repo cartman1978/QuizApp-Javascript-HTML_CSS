02a12908e3792afbd02388389081ca9b64dca0fb
cartman1978 <cartman_7802@yahoo.ie>
2021-03-15 09:48:45 +0000

    added high score functionality

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,6 +3,12 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -12,5 +18,16 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
     console.log('click save button!');
     e.preventDefault();
-    
-}
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort((a,b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+};
