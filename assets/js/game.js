28e1a9ffe49e0ae0c46aff5a6722b9b02c6be7b0
cartman1978 <cartman_7802@yahoo.ie>
2021-03-15 11:34:01 +0000

    added fetch data functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,18 +12,38 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch("questions.json")
-.then(res => {
-    return res.json();
-})
-.then(loadedQuestions => {
-   console.log(loadedQuestions);
-   questions = loadedQuestions;
-   startGame();
-})
-.catch(err => {
-    console.log(err);
-});
+fetch(
+    'https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple'
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
